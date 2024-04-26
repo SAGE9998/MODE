b81b0a9549335a71e349016fafa33e11fe6526b8
SAGE9998 <163633278+SAGE9998@users.noreply.github.com>
2024-04-26 13:25:46 +0700

    Add files via upload

---
+++ files/cf.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+MYIP=$(wget -qO- icanhazip.com);
+apt install jq curl -y
+clear
+echo -e ""
+echo -e "\033[96;1m============================\033[0m"
+echo -e "\033[93;1m      MEMBUAT DOMAIN RANDOM"
+echo -e "\033[96;1m============================\033[0m"
+echo -e ""
+DOMAIN=macro.my.id
+sub=$(</dev/urandom tr -dc a-z0-9 | head -c5)
+dns=${sub}.macro.my.id
+#(</dev/urandom tr -dc a-z0-9 | head -c5)
+dns=${sub}.macro.my.id
+CF_KEY=9a69eca3f44c5d60c0f0db9a307edec84527b
+CF_ID=Sanztunneling@gmail.com
+set -euo pipefail
+IP=$(wget -qO- icanhazip.com);
+echo "Updating DNS for ${dns}..."
+ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
+RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${dns}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
+if [[ "${#RECORD}" -le 10 ]]; then
+     RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"A","name":"'${dns}'","content":"'${IP}'","ttl":120,"proxied":false}' | jq -r .result.id)
+fi
+
+RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"A","name":"'${dns}'","content":"'${IP}'","ttl":120,"proxied":false}')
+echo "$dns" > /root/domain
+echo "$dns" > /root/scdomain
+echo "$dns" > /etc/xray/domain
+echo "$dns" > /etc/v2ray/domain
+echo "$dns" > /etc/xray/scdomain
+echo "IP=$dns" > /var/lib/kyt/ipvps.conf
+cd
